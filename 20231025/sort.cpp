4207cf46b6e31a7e39b5aefc62a87ea650b25af5
mon1210 <himejijin@i.softbank.jp>
2023-10-25 23:35:03 +0900

    ソ挿入ãï½ソートã作作成

---
+++ 20231025/sort.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+    // 宣言・初期化
+    int val[10] = {1,9,5,7,3,2,8,6,10,4};
+    int sortval[10] = {0};
+
+    int valueA;
+    int valueB;
+
+    int max;
+    int min;
+    
+    // ソート前出力
+    for(int i = 0; i < 10; i++)
+    {
+        cout << val[i];
+        cout << ",";
+    }
+    cout << "\n";
+
+    // ソート
+    for(int i = 0; i < 10; i++)
+     {
+        for(int j = i; j >= 0; j--)
+        {
+            if(sortval[j] == 0)
+            {
+                sortval[j] = val[i];
+            }
+            // 二週目
+            else if(sortval[j] > val[i])
+            {
+                // 前のインデックスから要素をコピー
+                sortval[j+1] = sortval[j];
+                // 空になった前のインデックスに次の要素を代入
+                sortval[j] = val[i];
+            }
+        }
+     }
+
+
+    cout << "\n";
+
+    // ソート後出力
+    cout << "sortval = ";
+    for(int i = 0; i < 10; i++)
+    {
+        cout << sortval[i];
+        cout << ",";
+    }
+    cout << "\n";
+
+}
